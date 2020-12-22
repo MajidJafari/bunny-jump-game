7a404fefa6cc0e70c92b44d46bdf1f5625ff94e2
majid <majid@DESKTOP-B04BH2K.localdomain>
2020-12-22 23:24:03 +0330

    feat: scrolling platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,8 @@ import Phaser from '../lib/phaser.js'
 export default class Game extends Phaser.Scene {
     /** @type {Phaser.Physics.Arcade.StaticBody} */
     player;
+    /** @type {Phaser.Physics.Arcade.StaticGroup} */
+    platforms;
 
     constructor() {
         super('game')
@@ -17,13 +19,12 @@ export default class Game extends Phaser.Scene {
     create() {
         this.add.image(240, 320, "background");
 
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
         for(let i = 0; i < 5; i++) {
             const x = Phaser.Math.Between(80, 400);
             const y = 150 * i;
             
-            /** @type {Phaser.Physics.Arcade.Sprite} */
-            const platform = platforms.create(x, y, "platform");
+            const platform = this.platforms.create(x, y, "platform");
             platform.scale = 0.5;
 
             /** @type {Phaser.Physics.Arcade.StaticBody} */
@@ -33,7 +34,7 @@ export default class Game extends Phaser.Scene {
 
         this.player = this.physics.add.image(240, 320, "player").setScale(0.5);
         this.cameras.main.startFollow(this.player);
-        this.physics.add.collider(platforms, this.player);
+        this.physics.add.collider(this.platforms, this.player);
         this.player.body.checkCollision = {
             up: false,
             down: true,
@@ -49,5 +50,16 @@ export default class Game extends Phaser.Scene {
         if(touchingDown) {
             this.player.setVelocityY(-300);
         }
+
+        /** @type {Phaser.Physics.Arcade.Sprite} */
+        this.platforms.children.iterate(platform => {
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const { scrollY } = this.cameras.main;
+
+            if(platform.y > scrollY + 700) {
+                platform.y = scrollY + Phaser.Math.Between(50, 100);
+                platform.body.updateFromGameObject();
+            }
+        });
     }
 }
